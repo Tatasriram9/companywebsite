dac079d375213baaca03c625ae8fdb022d6a629b
Tatasriram9 <sriram9.dev@gmail.com>
2025-02-04 14:32:12 +0530

    4th version

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -33,8 +33,8 @@ const NavBar = () => {
         <Navbar.Brand href="/home"  className={NavStyle.logoBrand}>
           <img
               src={Clogo}
-              width="30"
-              height="30"
+              width="50"
+              height="50"
               className="d-inline-block align-top"
               alt="React Bootstrap logo"
             />
